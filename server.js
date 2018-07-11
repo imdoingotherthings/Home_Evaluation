17b386adbb47fbacab47fc87a82e266d87d72938
Asai <Asai_Code@Asais-MacBook-Pro.local>
2018-07-11 00:39:13 -0700

    deleting hbs partials

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const hbs = require('hbs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-hbs.registerPartials(__dirname + '/views/partials');
-app.set('view engine', 'hbs');
-
 app.use(express.static(__dirname + '/public'));
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
